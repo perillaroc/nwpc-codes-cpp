6cb33413a77baed35be2e21863bbfccdcfb7b989
perillaroc <perillaroc@gmail.com>
2019-08-08 21:00:13 +0800

    :white_check_mark: check empty grib table.

--- example/grib_database/main.cpp
+++ example/grib_database/main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdint>
-#include <cmath>
+#include <cassert>
 
 #include <grib_property/grib_table_database.h>
 
 int main() {
 	GribCoder::GribTableDatabase db;
 	auto table = db.getGribTable("4", "4.1.0");
+	assert(table != nullptr);
+
+	auto missed_table = db.getGribTable("50", "1.1");
+	assert(missed_table == nullptr);
 
     return 0;
 }
